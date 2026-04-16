e7a356b6cf762ab9524d8cb3093293d6805801f5
eccyan <g00.eccyan@gmail.com>
2026-04-16 21:20:34 +0900

    refactor: remove WorldPortal+WorldInstance from WorldManifest (deprecation warnings)

--- include/gseurat/engine/world_manifest.hpp
+++ include/gseurat/engine/world_manifest.hpp
@@ -25,30 +25,11 @@ struct StreamingVolume {
     bool contains(const glm::vec3& point) const;
 };
 
-struct WorldPortal {
-    std::string id;
-    glm::vec3 position{0.0f};
-    std::string region_shape{"sphere"};
-    float region_radius{2.0f};
-    glm::vec3 region_half_extents{1.0f};
-    std::string target_instance_id;
-    glm::vec3 spawn_position{0.0f};
-    std::string spawn_facing;
-};
-
-struct WorldInstance {
-    std::string id;
-    std::string display_name;
-    std::string scene_file;
-};
-
 struct WorldManifest {
     int version{1};
     glm::vec3 grid_cell_size{64.0f, 32.0f, 64.0f};
     std::vector<WorldChunk> chunks;
     std::vector<StreamingVolume> streaming_volumes;
-    std::vector<WorldPortal> portals;
-    std::vector<WorldInstance> instances;
 
     std::pair<glm::vec3, glm::vec3> chunk_aabb(size_t index) const;
 
